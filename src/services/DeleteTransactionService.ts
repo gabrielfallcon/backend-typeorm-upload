2ee9689dc077dd7dc05eba9186f92a1fe5495a68
Gabriel Nascimento <gabriel.nascimenton.19@gmail.com>
2020-05-11 10:18:55 -0300

    :heavy_plus_sign: criando tratativa de erros global

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: RequestDTO): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const findId = await transactionRepository.findOne(id);
+
+    if (!findId) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(findId);
   }
 }
 
